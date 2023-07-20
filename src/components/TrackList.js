5e5f460623c945a7051a8aaf57cdde17dfa654c4
Truc Tran <tracytrle@gmail.com>
2023-07-19 22:05:51 -0500

    fix button on tracklist

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -4,7 +4,8 @@ import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
 import { IconContext } from "react-icons";
 
 function TrackList() {
-  const { trackList, currentTrackIndex, playTrack } = useMusicPlayer();
+  const { trackList, currentTrackIndex, playTrack, isPlaying } =
+    useMusicPlayer();
 
   return (
     <div className="track-list">
@@ -17,13 +18,13 @@ function TrackList() {
             playTrack(index);
           }}
         >
-          {currentTrackIndex === index ? (
-            <IconContext.Provider value={{ size: "3em", color: "#7f8c8d" }}>
-              <AiFillPlayCircle />
+          {currentTrackIndex === index && isPlaying ? (
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+              <AiFillPauseCircle />
             </IconContext.Provider>
           ) : (
             <IconContext.Provider value={{ size: "3em", color: "#7f8c8d" }}>
-              <AiFillPauseCircle />
+              <AiFillPlayCircle />
             </IconContext.Provider>
           )}
           <p>{track.name}</p>
